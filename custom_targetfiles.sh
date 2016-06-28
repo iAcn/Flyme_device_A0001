e92eaf703387c5e2f168da83f88192a63bdc2c4a
meolunr <meolunr@gmail.com>
2016-06-28 14:31:45 +0800

    Add double kernel switch

--- custom_targetfiles.sh
+++ custom_targetfiles.sh
@@ -9,3 +9,15 @@ cp -f other/SuperSU.zip $TARGET_DIR/META
 
 cd $TARGET_DIR/SYSTEM
 rm -rf tts
+
+mkdir stocksettings/KernelFile
+
+cd ../BOOTABLE_IMAGES
+
+unpack_bootimg boot.img newBoot
+cp -f ../../../other/img_info newBoot/
+pack_bootimg newBoot PrefKernel.img
+rm -rf newBoot
+
+mv PrefKernel.img ../SYSTEM/stocksettings/KernelFile
+cp -f boot.img ../SYSTEM/stocksettings/KernelFile/DefKernel.img
